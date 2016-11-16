81c6edb198ed266197cc1ab120a9091f66ba68c3
Jungmin Cha <ckwjdals90@gmail.com>
2016-11-16 00:42:09 +0000

    counter function created

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function() {
+// $( document ).on("ready", function() {
+// $( document ).ready(function() {
+$(function() {
+
+  $( "#tweet-text" ).on("keyup", function() {
+    var inputLength = $(this).val().length;
+    var counterOutput = $(".counter");
+    var counterValue = 140 - inputLength;
+    counterOutput.text(counterValue);
+    }
+  });
+
+});
